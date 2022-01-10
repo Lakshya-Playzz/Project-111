b9041da6c37964e014708953855392fefba4f3b4
Lakshya-Playzz <85876093+Lakshya-Playzz@users.noreply.github.com>
2022-01-10 17:07:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+//https://teachablemachine.withgoogle.com/models/1i7iqWhv0/
+Prediction1 = ""
+Prediction2 = ""
+
+Webcam.set({
+    height : 300,
+    width : 350,
+    image_format :"png",
+    png_quality : 100
+})
+camera = document.getElementById("camera")
+Webcam.attach("camera");
+ 
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img src = '"+data_uri+"' id = 'captured_image'>"
+    })
+    
+    
+}
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1i7iqWhv0/modl.json" , modelLoaded)
+function modelLoaded(){
+    console.log("model_is_loaded")
+
+}
+function check(){
+    img = document.getElementById("captured_image")
+    classifier.classify(img , gotResult)
+
+}
+function gotResult(error , result){
+if(error){
+console.error(error)
+}
+else{
+    console.log(result)
+    Prediction1 = result[0].label;
+    Prediction2 = result[1].label;
+    document.getElementById("hand_gestures_name_1").innerHTML = Prediction
+    
+
+    speak()
+}
+
+}
+function speak(){
+    data = " The Prediction is " + Prediction
+    var synth = window.speechSynthesis;
+    var utterThis = window.SpeechSynthesisUtterance(data1 + data2)
+    synth.speak(utterThis) 
+}
+
+    
+    
+
+
